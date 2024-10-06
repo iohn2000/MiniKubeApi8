a97dcc612fdbc53569dd3f092e989b666e341194
Johannes <johannes.fleck@gmail.com>
2024-10-06 23:00:05 +0200

    Weatherservice with secrets (#5)
    * fixing helm charts
    started added appsettings and secrets demo
    not working yet
    
    * improve helper scripts
    
    * introduce a appsettins.local.json settings file.
    a secret stored in kubernetes is mounten into app directory to provide secret settins
    
    * added script to create a secret
    
    * use helm set var=value command to set environment uat or prod
    
    ---------
    
    Co-authored-by: FleckJ <johannes.fleck@k-business.com>

--- build_and_push_helm.ps1
+++ build_and_push_helm.ps1
@@ -1,4 +1,4 @@
 & minikube -p minikube docker-env --shell powershell | Invoke-Expression
 docker build -t localhost:5000/mykube-img .
 docker push localhost:5000/mykube-img
-helm upgrade weather .\helm
+helm upgrade weather .\helm --set environment=uat 
--- tmp/createSecret.ps1
+++ tmp/createSecret.ps1
@@ -1,1 +1,7 @@
-kubectl create secret generic appsettings-secret --from-file=appsettings.local.json=secret-config.json
+& minikube -p minikube docker-env --shell powershell | Invoke-Expression
+
+kubectl delete secret appsettings-secret-prod
+kubectl delete secret appsettings-secret-uat 
+
+kubectl create secret generic appsettings-secret-prod --from-file=secret-prod-config.json=secret-prod-config.json
+kubectl create secret generic appsettings-secret-uat --from-file=secret-uat-config.json=secret-uat-config.json
