abf4efa35e33e9d7e11a678735ebefd7d43ec60f
Johannes <johannes.fleck@gmail.com>
2024-10-08 20:45:29 +0200

    Weatherservice with versioning via nbgv (#6)
    update script to set appVersion via nbgv

--- build_and_push_helm.ps1
+++ build_and_push_helm.ps1
@@ -1,4 +1,11 @@
 & minikube -p minikube docker-env --shell powershell | Invoke-Expression
 docker build -t localhost:5000/mykube-img .
 docker push localhost:5000/mykube-img
-helm upgrade weather .\helm --set environment=uat 
+
+$v =  nbgv get-version -v SemVer1
+helm package helm --app-version $v # only helm package can set appVersion
+
+
+helm upgrade weather .\my-minikube-weather-api-0.4.0.tgz --set environment=uat
+
+#helm upgrade weather .\helm --app-version $v --set environment=uat 
