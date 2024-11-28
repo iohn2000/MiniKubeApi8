c603e69bb30a4a27ec43364e7d79296eb7034cc4
Johannes <johannes.fleck@gmail.com>
2024-11-28 12:15:50 +0100

    Weatherservice with secrets (#7)
    * fixing helm charts
    started added appsettings and secrets demo
    not working yet
    
    * improve helper scripts
    
    * introduce a appsettins.local.json settings file.
    a secret stored in kubernetes is mounten into app directory to provide secret settins
    
    * added script to create a secret
    
    * use helm set var=value command to set environment uat or prod
    
    * update script to set appVersion via nbgv
    
    * small improvements
    
    * improve setup.ps1
    
    ---------
    
    Co-authored-by: FleckJ <johannes.fleck@k-business.com>

--- build_and_push_helm.ps1
+++ build_and_push_helm.ps1
@@ -5,7 +5,4 @@ docker push localhost:5000/mykube-img
 $v =  nbgv get-version -v SemVer1
 helm package helm --app-version $v # only helm package can set appVersion
 
-
-helm upgrade weather .\my-minikube-weather-api-0.4.0.tgz --set environment=uat
-
-#helm upgrade weather .\helm --app-version $v --set environment=uat 
+helm upgrade --install weather .\my-minikube-weather-api-0.4.0.tgz --set environment=uat
