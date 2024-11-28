d6d78f2f46f320f979bb99e681080f5ebfe3eba8
FleckJ <johannes.fleck@k-business.com>
2024-11-28 12:14:02 +0100

    improve setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -4,6 +4,8 @@ $dashboard = Start-Job -ScriptBlock { minikube dashboard }
 $forward = Start-Job -ScriptBlock { kubectl port-forward --namespace kube-system service/registry 5000:80 }
 $tunnel = Start-Job -ScriptBlock { minikube tunnel }
 $registry = Start-Job -ScriptBlock { Start-Process "msedge.exe" " http://localhost:5000/v2/_catalog" }
+Get-Job
+# Stop-Job -Id $dashboard.Id, $forward.Id, $tunnel.Id, $registry.Id
+# Remove-Job -Id $dashboard.Id, $forward.Id, $tunnel.Id, $registry.Id
+
 # Get-Job -Id $job.Id
-# Stop-Job -Id $job.Id
-# Remove-Job -Id $job.Id
